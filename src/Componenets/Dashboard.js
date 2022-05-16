5a1640196616c018f2c9dd1848ac927745355307
akshatkushwaha <akshat.official999@gmail.com>
2022-05-16 14:56:12 +0530

    token accessed, need to resolve data handling

--- src/Componenets/Dashboard.js
+++ src/Componenets/Dashboard.js
@@ -1,34 +1,33 @@
-import { Component } from "react";
 import axios from "axios";
 
-class Dashboard extends Component {
-  constructor(props) {
-    super(props);
-    this.HandleGoogleLogin = this.handleGoogleLogin.bind(this);
-  }
-
-  handleGoogleLogin = async () => {
-    try {
-      const response = await axios.get("/dashboard/");
-      console.log(response);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  render() {
-    return (
-      <>
-        <section className="absolute w-full h-full bg-gray-150">
-          <div className="container mx-auto py-10 px-10 h-full">
-            <div className="flex content-center justify-center h-full">
-              <h1 className="text-2xl">Profile Details</h1>
-            </div>
-          </div>
-        </section>
-      </>
+async function getData() {
+  try {
+    var url = window.location;
+    var access_token = url.hash.substring(1);
+    const response = await axios.get(
+      `https://www.googleapis.com/oauth2/v2/userinfo?${access_token}`
     );
+    // await axios.post("/api/googleauth", response.data);
+    return response;
+  } catch (error) {
+    console.log(error);
   }
+}
+
+function Dashboard() {
+  getData().then((response) => console.log(response));
+  return (
+    <>
+      <section className="absolute w-full h-full bg-gray-150">
+        <div className="container mx-auto py-10 px-10 h-full">
+          <div className="flex content-center justify-center h-full">
+            <h1 className="text-2xl">Profile Details</h1>
+            {/* <h1>Name : {data}</h1> */}
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Dashboard;
--- src/Componenets/GoogleButton.js
+++ src/Componenets/GoogleButton.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class GoogleButton extends Component {
   render() {
     return (
-      <a href="https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=https%3A%2F%2Flocalhost%3A3000%2Fdashboard%2F&prompt=consent&response_type=code&client_id=605425009699-a08v212752nbljkldsado1odec6d8nfk.apps.googleusercontent.com&scope=Profile&access_type=offline">
+      <a href="https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=https%3A%2F%2Flocalhost%3A3000%2Fdashboard%2F&prompt=consent&response_type=token&client_id=605425009699-a08v212752nbljkldsado1odec6d8nfk.apps.googleusercontent.com&scope=Profile&access_type=online">
         <button
           className="bg-white active:bg-gray-100 text-gray-800 px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs"
           type="button"
